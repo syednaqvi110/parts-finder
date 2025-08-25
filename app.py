ccdd385665e41c399a5ef6854cae44aac72eef70
syednaqvi110 <wajahathussain286@hotmail.com>
2025-08-25 15:58:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from io import StringIO
 # ============================================================================
 # SIMPLE CONFIGURATION - JUST CHANGE THE URL BELOW
 # ============================================================================
-PARTS_DATABASE_URL = "https://docs.google.com/spreadsheets/d/1_dnPBZUolOMrruvQIv_GcpvGurgoU09FvPlvMwhgpG0/edit?usp=sharing"
+PARTS_DATABASE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc2GTX3jc2NjJlR_zWVqDyTGf6bhCVc4GGaN_WMQDDlXZ8ofJVh5cbCPAD0d0lHY0anWXreyMdon33/pub?output=csv"
 
 # Basic settings (you can adjust these if needed)
 RESULTS_PER_PAGE = 15           # How many results to show per page
